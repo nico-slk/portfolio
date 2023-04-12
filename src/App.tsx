c8f9872ac16e699ccea15565280323be32635905
nico-slk <nico1991.6@gmail.com>
2023-04-12 17:47:51 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div>
+        <a href="" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Project PORTFOLIO init</h1>
+    </div>
+  )
+}
+
+export default App
