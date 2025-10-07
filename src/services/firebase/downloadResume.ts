627fb7669af961b810ef9925c3e197beb59e82f1
nico-slk <62625216+nico-slk@users.noreply.github.com>
2025-10-07 16:14:41 -0300

    Update downloadResume.ts

--- src/services/firebase/downloadResume.ts
+++ src/services/firebase/downloadResume.ts
@@ -2,7 +2,7 @@ import { getDownloadURL, ref } from 'firebase/storage';
 import { storage } from './firebase';
 
 export const downloadResume = async (): Promise<string> => {
-  const gsReference = ref(storage, 'gs://portfolio-slk2023.appspot.com/resume/Nicolas Selicki CV 2023 EN.pdf');
+  const gsReference = ref(storage, 'gs://portfolio-slk2023.appspot.com/resume/Nicolas Selicki CV EN.pdf');
   let urlFile = ''
 
   await getDownloadURL(gsReference)
